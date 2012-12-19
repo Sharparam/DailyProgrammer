b31971a434e864dfb00eaa0a57317eb697f24a56
F16Gaming <adam.hellberg95@gmail.com>
2012-12-19 11:06:50 +0100

    Added Challenge113Easy

---
+++ Challenge113/Challenge113Easy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Challenge113Easy
+{
+	public class Program
+	{
+		private static readonly Regex IntRegex = new Regex(@"^[+-]?\d+$", RegexOptions.Compiled);
+		private static readonly Regex FloatRegex = new Regex(@"^[+-]?\d+[\.,]\d+$", RegexOptions.Compiled);
+		private static readonly Regex DateRegex = new Regex(@"^\d{1,2}-\d{1,2}-(?:\d{2}|\d{4})$", RegexOptions.Compiled);
+
+		static void Main()
+		{
+			var quit = false;
+			while (!quit)
+			{
+				var input = Utilities.Input.GetInput("Write something ('quit' to quit):", "Write something ('quit' to quit):", s => !string.IsNullOrEmpty(s));
+				if (input == "quit")
+					quit = true;
+				else if (IntRegex.IsMatch(input))
+					Console.WriteLine("int");
+				else if (FloatRegex.IsMatch(input))
+					Console.WriteLine("float");
+				else if (DateRegex.IsMatch(input))
+					Console.WriteLine("date");
+				else
+					Console.WriteLine("string");
+			}
+		}
+	}
+}
