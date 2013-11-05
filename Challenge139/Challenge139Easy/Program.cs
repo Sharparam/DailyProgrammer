4f199bc7fdd5aa362b7d6e7c0193ed15d18cfbbf
Sharparam <adam.hellberg95@gmail.com>
2013-11-05 12:34:57 +0100

    Added Challenge 139 (Easy)

---
+++ Challenge139/Challenge139Easy/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Challenge #139 [Easy] (Pangrams)
+ * http://www.reddit.com/r/dailyprogrammer/comments/1pwl73/11413_challenge_139_easy_pangrams/
+ */
+
+using System;
+using System.Linq;
+
+namespace Challenge139Easy
+{
+    public class Program
+    {
+        private const string Alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var num = int.Parse(input);
+            var lines = new string[num];
+            for (var i = 0; i < num; i++)
+                lines[i] = Console.ReadLine().ToLower();
+
+            foreach (var line in lines)
+            {
+                Console.Write(Alphabet.All(c => line.Contains(c)) ? "True: " : "False: ");
+
+                Console.WriteLine(String.Join(", ", Alphabet.Select(l => String.Format("{0}: {1}", l, line.Count(c => c == l)))));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
